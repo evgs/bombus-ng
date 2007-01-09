f861fe593046998678327e88fa130964b060d153
Eugene Stahov <evgs@bombus-im.org>
2007-01-09 21:33:17 +0000

    bombus-ng.cpp - discarding all code - obsolete Image.cpp - processing virtual-alpha channel. no AlphaBlending() support on WindowsCE below 5.0 :(

--- src/Image.cpp
+++ src/Image.cpp
@@ -10,11 +10,53 @@ Image::~Image() {
     if (bmp) DeleteObject(bmp);
 }
 
+
+int getColor(BITMAP &bm, int x, int y) {
+    LPCHAR p=(LPCHAR) bm.bmBits+y*bm.bmWidthBytes;
+    switch (bm.bmBitsPixel) {
+        case 8: p+=x; return *p; 
+        case 16: p+=x*2; return *((LPWORD)p); 
+        case 24: p+=x*3; return *((LPWORD)p)  | *(p+2) ; 
+        case 32: p+=x*4; return *((LPDWORD)p); 
+    }
+    return 0;
+}
+
 void Image::drawImage( HDC hdc, int x, int y ) const {
     BITMAP bm;
     GetObject(bmp, sizeof(bm), &bm);
     HDC src=CreateCompatibleDC(NULL);
     SelectObject(src, bmp);
-    BitBlt(hdc, x,y, bm.bmWidth, bm.bmHeight, src, 0,0, SRCCOPY);
+
+    //char *bits=new char()
+
+    int bmaskWB=bm.bmWidth/8;
+    char *bmask=new char[bmaskWB*bm.bmHeight];
+
+    int maskColor=getColor(bm, 0, 0);
+
+    LPCHAR p=bmask;
+    {for (int y=bm.bmHeight; y>0; y--) {
+        for (int xb=0; xb<bmaskWB; xb++) {
+            char buf;
+            for (char b=0; b<8; b++) {
+                buf=(buf<<1) | ((getColor(bm, xb*8+b, y-1)==maskColor)? 0:1);
+            }
+            *(p++)=buf;
+        }
+    }}
+    HBITMAP mask=CreateBitmap( bm.bmWidth, bm.bmHeight, 1, 1, bmask);
+
+    delete bmask;
+
+    //BitBlt(hdc, x,y, bm.bmWidth, bm.bmHeight, src, 0,0, SRCAND);
+    //BitBlt(hdc, x,y, bm.bmWidth, bm.bmHeight, src, 0,0, SRCPAINT);
+    MaskBlt(hdc, x,y, bm.bmWidth, bm.bmHeight, 
+            src, 0,0, 
+            mask, 0, 0, 
+            MAKEROP4(SRCCOPY, 0x00AA0029));
+    //AlphaBlend(hdc, x,y, bm.bmWidth, bm.bmHeight, src, 0,0, bm.bmWidth, bm.bmHeight, )
+    DeleteObject(mask);
     DeleteDC(src);
 }
+

bcb4625f658a870d5e0bd4dc842d817a0f1ab37c
Eugene Stahov <evgs@bombus-im.org>
2007-01-21 19:34:01 +0000

    Serialize class for streaming application non-volatile data JabberAccount serialization - loading and saving (still single account)

--- JabberAccount.cpp
+++ JabberAccount.cpp
@@ -3,14 +3,20 @@
 #include "JabberAccount.h"
 
 JabberAccount::JabberAccount(){
+    init();
+}
+void JabberAccount::init(){
+    version=1;
+
 	password="";
 
     //useSRV=true;
 	hostNameIp="";
 	port=5222;
+    setResource("bombus-ng");
 
 	plainTextPassword=false;
-	useSASL=false;
+	useSASL=true;
 	useEncryption=false;
     legacySSL=false;
 	useCompression=false;
@@ -19,20 +25,50 @@ JabberAccount::JabberAccount(){
 }
 
 JabberAccount::JabberAccount(const std::string &bareJid, const std::string &resource) {
-	password="";
-
-    //useSRV=true;
-    hostNameIp="";
-	port=5222;
-
-	plainTextPassword=false;
-	useSASL=false;
-	useEncryption=false;
-	legacySSL=false;
-    useCompression=false;
-	useProxy=false;
+    init();
 	setBareJid(bareJid);
 	setResource(resource);
 }
 
+JabberAccount::JabberAccount( LPCTSTR filename ) {
+    init();
+    Serialize s(filename, true);
+    serialize(s, true);
+}
+
 JabberAccount::~JabberAccount(){}
+
+void JabberAccount::saveAccount( LPCTSTR fileName ) {
+    Serialize s(fileName, false);
+    serialize(s, false);
+}
+
+void JabberAccount::serialize( Serialize &s, bool read ) 
+{
+    //////////////////////////////////////////////////////////////////////////
+    // Serialization
+    int version=this->version;
+    s.streamInt(version);
+
+    std::string sjid=getJid();
+    s.streamString(sjid);
+    s.streamScrambledString(password); 
+
+    // host data
+    s.streamString(hostNameIp);
+    s.streamInt(port);
+
+    // login settings
+    s.streamBool(plainTextPassword);
+    s.streamBool(useSASL);
+    s.streamBool(useEncryption);
+    s.streamBool(legacySSL);
+    s.streamBool(useCompression);
+
+    // proxy
+    s.streamBool(useProxy);
+    s.streamString(proxy);
+
+    setJid(sjid);
+}
+

fc89d75cc62d16956bcefb4db46146284d3af5be
Eugene Stahov <evgs@bombus-im.org>
2007-02-12 18:49:23 +0000

    transport icons used in roster

--- Contact.cpp
+++ Contact.cpp
@@ -3,12 +3,29 @@
 #include "Jid.h"
 #include "Contact.h"
 #include "utf8.hpp"
+#include "Image.h"
+
+//////////////////////////////////////////////////////////////////////////
+extern ImgListRef skin;
+
+int identifyTransport(const std::string &jid) {
+    int serv=jid.find('@')+1;
+    int dotsrv=jid.find('.', serv);
+    if (dotsrv<0) return 0;
+
+    Skin * il= dynamic_cast<Skin *>(skin.get());
+    if (!il) return 0;
+    return il->getBaseIndex(jid.substr(serv, dotsrv-serv));
+}
+//////////////////////////////////////////////////////////////////////////
 
 Contact::Contact(const std::string &jid, const std::string &resource, const std::string &nickname) {
     this->jid=Jid(jid, resource);
     this->rosterJid=jid;
     this->nickname=nickname;
     this->status=presence::OFFLINE;
+
+    transpIndex=identifyTransport(jid);
 
     update();
     messageList=ODRListRef(new ODRList);
@@ -24,7 +41,7 @@ bool Contact::hasUnreadMsgs() {
     return false;
 }
 int Contact::getColor() const{ return 0; }
-int Contact::getIconIndex() const{ return status; }
+int Contact::getIconIndex() const{ return status+transpIndex; }
 
 const wchar_t * Contact::getText() const{ return wjid.c_str(); }
 //////////////////////////////////////////////////////////////////////////
--- Image.cpp
+++ Image.cpp
@@ -3,6 +3,7 @@
 #include <aygshell.h>
 
 #include <string>
+#include <utf8.hpp>
 
 #define NORASTEROP (0x00AA0029)
 
@@ -57,6 +58,7 @@ void ImgArray::setGridSize( int nColumns, int nRows ) {
 ImgArray::ImgArray( LPCTSTR path, int nColumns, int nRows ) {
     std::wstring bmpPath=appRootPath+path;
     bmp=SHLoadImageFile(bmpPath.c_str()); 
+    //if (bmp==NULL) throw
 
     HDC hdcImage=CreateCompatibleDC(NULL);
     SelectObject(hdcImage, bmp);
@@ -68,4 +70,51 @@ ImgArray::ImgArray( LPCTSTR path, int nColumns, int nRows ) {
 
 ImgArray::~ImgArray(){
     if (bmp) DeleteObject(bmp);
+}
+
+//////////////////////////////////////////////////////////////////////////
+Skin::Skin( LPCTSTR path )  {
+    names.push_back("");
+    iconset.push_back(ImgListRef(new ImgArray(TEXT("skin.png"), 8, 6)));
+
+    std::wstring srcPath=appRootPath+path+TEXT("transports\\*.*");
+
+    WIN32_FIND_DATA wfd;
+
+    HANDLE sf=FindFirstFile(srcPath.c_str(), &wfd);
+    if (sf==INVALID_HANDLE_VALUE) return;
+    do {
+        std::wstring wname(path);
+        wname+=TEXT("transports\\");
+        wname+=wfd.cFileName;
+        iconset.push_back(ImgListRef(new ImgArray(wname.c_str(), 8, 1)));
+
+        std::string name=utf8::wchar_utf8(wfd.cFileName);
+        int dot=name.find('.');
+        name.erase(dot, name.length()-dot);
+        names.push_back(name);
+    } while (FindNextFile(sf, &wfd));
+    FindClose(sf);
+}
+
+void Skin::drawElement( HDC hdc, int index, int x, int y ) const {
+    unsigned int sIndex=index>>8;
+    if (sIndex>=iconset.size()) return;
+    iconset[sIndex]->drawElement(hdc, index &0xff, x, y);
+}
+
+int Skin::getElementWidth() const{
+    return iconset.front()->getElementWidth();
+}
+
+int Skin::getElementHeight() const {
+    return iconset.front()->getElementHeight();
+}
+Skin::~Skin() {}
+
+int Skin::getBaseIndex( const std::string &setName ) {
+    for (int i=names.size()-1; i>0; i--) {
+        if (setName==names[i]) return i<<8;
+    }
+    return 0;
 }

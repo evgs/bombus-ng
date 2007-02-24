bdbe0c0b69746640697425950d3b3fb37f60ecea
Eugene Stahov <evgs@bombus-im.org>
2007-02-24 16:54:40 +0000

    correct processing contacts with different resources

--- include/Contact.h
+++ include/Contact.h
@@ -13,25 +13,12 @@
 
 #include "VirtualListView.h"
 
-/*enum Status {
-    PRESENCE_ONLINE=0,
-    PRESENCE_CHAT=1,
-    PRESENCE_AWAY=2,
-    PRESENCE_XA=3,
-    PRESENCE_DND=4,
-    PRESENCE_OFFLINE=5,
-    PRESENCE_ASK=6,
-    PRESENCE_UNKNOWN=7,
-    PRESENCE_INVISIBLE=8,
-    PRESENCE_ERROR=9,
-    PRESENCE_TRASH=10,
-    PRESENCE_AUTH=-1
-};*/
-
 class Contact : public IconTextElement {
 public:
     typedef boost::shared_ptr<Contact> ref;
     Contact(const std::string &jid, const std::string &resource, const std::string &nickname);
+
+    Contact::ref clone();
 
     Jid jid;
     std::string nickname;
